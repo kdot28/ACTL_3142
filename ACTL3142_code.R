a2b8855c4e95a5e324fa3b319cbc810d960662b5
kdot28 <deshwal_k@yahoo.com>
2022-07-11 23:30:53 +1000

    lolol

--- ACTL3142_code.R
+++ ACTL3142_code.R
@@ -217,7 +217,7 @@ Claims_freq_3 <- Claims_freq_2 %>%
 
 GLM_data2 <- cbind(Claims_freq_3, CPI, Fuel_movement, Transport_CPI,
                    JPY_AUD, Avg_sum_insured = Sum_insured_quarterly$Avg_sum_insured)
-colnames(GLM_data2) <- c("Accident_Quarter", "Claims_Freq", "CPI", 
+colnames(GLM_data2) <- c("Accident_Quarter", "Claims_Freq","Exposure", "CPI", 
                          "Quarterly.Change", "Transport.CPI", "Exchange.Rate", 
                          "Avg_sum_insured")
 
@@ -225,7 +225,7 @@ glm_freq <- glm(Claims_Freq ~  Transport.CPI +
                  Avg_sum_insured + Exchange.Rate, 
                  data = GLM_data2, 
                  family = poisson(link = "log"),
-                 offset = log(exp))
+                 )
 summary(glm_freq)
 
 set.seed(10101)
@@ -235,4 +235,9 @@ for (j in 1:10) {glm_sev
 }
 kfold_error_10
 mean((GLM_data2$Claims_Freq - predict.glm(glm_freq))^2)/10
+
+plot(glm_sev)
+
+
+
 
