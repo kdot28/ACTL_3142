bb885ff158fcdb825a605f5fad4716578ff28ea5
kdot28 <deshwal_k@yahoo.com>
2022-07-31 15:31:27 +1000

    ko

--- ACTL3142_code.R
+++ ACTL3142_code.R
@@ -188,6 +188,79 @@ df$no_active_policies<- table(Commercial$accident_month)
 
 
 
+# DATA VISUALISATION
+
+#CLAIMS PER CLASS
+ggplot(Claims_by_class) + 
+  geom_bar(aes(x=reorder(vehicle_class, -No_claims), y= No_claims, fill = No_claims), stat = "identity") +
+  labs(x = "Vehicle Class", y = "Number of Claims",
+       title = "Number of Claims per Vehicle Class")+
+  scale_fill_gradient(low = "green", high = "orange")
+
+
+#AVE COST PER CLASS
+ggplot(Claims_by_class) + 
+  geom_bar(aes(x=reorder(vehicle_class, -Ave_Claim_Amt), y=Ave_Claim_Amt, fill = Ave_Claim_Amt), 
+           stat = "identity")+
+  theme_grey()+
+  scale_fill_gradient(low = "green", high = "orange")+
+  labs(x = "Vehicle Class", y = "Average Claim Amount",
+       title = "Average Claim Amount per Vehicle Class")
+
+
+
+#POLICYHOLDERS PER STATE (TABLE)
+PH_per_state <- table(Insurance_by_ID$State)
+
+library(data.table)
+PH_per_state
+
+
+#QUARTERLY NUMBER OF CLAIMS
+
+
+#Total Number of Claims per Month
+ggplot()+
+  geom_line(dat = Claims_per_month, aes(x = accident_month, y=Number_of_claims), 
+            stat = "identity") + 
+  labs(x = "Accident Month", y = "Total Number of Claims",
+       title = "Total Number of Claims per Month")+
+  theme_gray()
+
+#Average Claim Size per Month
+ggplot()+
+  geom_rect(data =Claims_per_month, aes(xmin = as.Date("2016-07-31"), ymin = -Inf, xmax = as.Date("2020-02-29"), ymax = Inf,
+  ))+
+  geom_rect(data =Claims_per_month, aes(xmin = as.Date("2020-02-29"), ymin = -Inf, xmax = as.Date("2021-06-30"), ymax = Inf,
+  ))+
+  geom_line(data = Claims_per_month,aes(x = accident_month, y=Average_claim_size), 
+            stat = "identity")+
+  labs(x = "Accident Month", y = "Average Claim Size",
+       title = "Average Claim Size per Month")
+
+
+#SUM INSURED BY MONTH
+month <- Commercial%>%
+  group_by(accident_month) %>%
+  summarise(amt = mean(sum_insured))
+
+month$accident_month<- as.Date(month$accident_month)
+
+ggplot(month)+
+  geom_line(aes(x = accident_month, y = amt), stat = "identity")+
+  labs(x = "Month", y = "Average Sum Insured",
+       title = "Average Sum Insured per Month")
+
+#Average Claim Cost per State
+
+ggplot(States) + 
+  geom_bar(aes(x=reorder(risk_state_name, -Sum_per_vehicle), y = Sum_per_vehicle, fill = Sum_per_vehicle),
+           stat = "identity") + 
+  scale_fill_gradient(low = "green", high = "orange")+
+  theme_gray()+
+  labs(x = "State Name", y = "Average Claim Cost",
+       title = "Average Claim Cost per State", fill = "Average Claim Cost")
+
 
 
 
